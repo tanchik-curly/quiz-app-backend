961dea120784a11b3acee555b5919160e84706b2
unknown <tetiana.diakun.pz.2019@lpnu.ua>
2021-06-22 16:57:33 +0300

    make db settings

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
+const { urlencoded } = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const userRouter = require("./routes/user");
 
+const jwt = require("jsonwebtoken");
+
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(userRouter);
 
