10f6d38b8513fbc65656b2ebb3d2c4a24c988b57
unknown <tetiana.diakun.pz.2019@lpnu.ua>
2021-07-02 12:07:37 +0300

    added read/write operations

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,20 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
-  const token = req.headers["x-access-token"];
-
-  if (!token) {
-    res.status(400).send("You do not have access token, login again!");
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log("TOKEN " + token);
+  if (token === "null") {
+    res.status(401).send("You do not have access token, login again!");
   } else {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) res.json({ auth: false, message: "Your token is invalid!" });
-      else next();
+      if (err) {
+        console.log(err);
+        res.json({ auth: false, message: "Your token is invalid!" });
+      } else {
+        req.userId = decoded.id;
+        next();
+      }
     });
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,13 @@ const jwt = require("jsonwebtoken");
 
 import models from "../models";
 import validateToken from "../middlewares/auth";
+import Questions from "../models/questions";
+
+//--------------------------------------
+//registration/authorization
 
 const genereteToken = (id) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: 1000,
-  });
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
   return token;
 };
 
@@ -18,7 +20,7 @@ router.post("/auth/login", async (req, res) => {
   models.User.findOne({ where: { email: email } })
     .then((user) => {
       if (password !== user.dataValues.password)
-        res.status(400).send("Password is invalid!");
+        res.status(400).json({ auth: false, message: "Password is invalid!" });
       else {
         const id = user.dataValues.id;
         const token = genereteToken(id);
@@ -55,6 +57,148 @@ router.post("/auth/register", async (req, res) => {
       message: "Your data are incorrect, try again!",
     });
   }
+});
+
+//---------------------------------------
+//get quizzes data
+
+router.get("/quizzes", async (req, res) => {
+  await models.Quizzes.findAll({
+    include: [
+      {
+        model: models.Questions,
+        include: [{ model: models.Options }],
+      },
+    ],
+  })
+    .then((quizzes) => {
+      res.json({ data: quizzes });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+});
+
+router.get("/quiz/:id", validateToken, async (req, res) => {
+  await models.Quizzes.findOne({
+    where: { id: req.params.id },
+    include: [
+      {
+        model: models.Questions,
+        include: [
+          {
+            model: models.Options,
+            attributes: ["option", "id"],
+          },
+        ],
+      },
+    ],
+  })
+    .then((quiz) => {
+      res.json({ auth: true, data: quiz });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ auth: false, data: err });
+    });
+});
+
+router.post("/options", validateToken, async (req, res) => {
+  const responseArray = [];
+  const response = [];
+  let length = req.body.options.length;
+
+  await req.body.options.forEach((option) => {
+    models.Options.findAll({
+      where: {
+        questionId: option.id,
+        correctOption: true,
+      },
+    })
+      .then((quiz) => {
+        let trueFlag = false;
+        let falseFlag = false;
+        let qId = null;
+
+        quiz.forEach((dbOption) => {
+          qId = dbOption.dataValues.questionId;
+
+          if (dbOption.dataValues.option === option.value) {
+            trueFlag = true;
+            response.push({ id: dbOption.dataValues.id, option: true });
+
+            //UNCOMMENT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+            // const review = models.QuizReview.create({
+            //   optionId: dbOption.dataValues.id,
+            //   answer: true,
+            //   questionId: qId,
+            //   quizId: req.body.quizId,
+            //   userId: req.userId,
+            // });
+          } else {
+            falseFlag = true;
+            response.push({ id: dbOption.dataValues.id, option: false });
+
+            // const review = models.QuizReview.create({
+            //   optionId: dbOption.dataValues.id,
+            //   answer: false,
+            //   questionId: qId,
+            //   quizId: req.body.quizId,
+            //   userId: req.userId,
+            // });
+          }
+        });
+        if (trueFlag && falseFlag === false)
+          responseArray.push({
+            questionId: qId,
+            correctOption: response,
+            message: "Correct!",
+          });
+        else if (falseFlag && trueFlag === false)
+          responseArray.push({
+            questionId: qId,
+            correctOption: response,
+            message: "Sorry, wrong answer!",
+          });
+        else
+          responseArray.push({
+            questionId: qId,
+            correctOption: response,
+            message: "Partly correct answer!",
+          });
+
+        --length;
+
+        if (length <= 0) {
+          res.json({
+            response: responseArray,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ auth: false, data: err });
+      });
+  });
+});
+
+//----------------------------------------------
+//check on completed quiz
+
+router.get("/scores/:id", validateToken, async (req, res) => {
+  await models.QuizReview.findAll({
+    where: { quizId: req.params.id, userId: req.userId },
+    attributes: ["answer", "optionId", "questionId"],
+  })
+    .then((quiz) => {
+      res.json({ auth: true, data: quiz });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ auth: false, data: err });
+    });
 });
 
 module.exports = router;
